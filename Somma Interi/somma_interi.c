c90944c23ac6f2cc40baa8f6b99e6e69f860b283
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2022-09-21 13:59:35 +0200

    Add files via upload

---
+++ Somma Interi/somma_interi.c
@@ -0,0 +1,30 @@
+/*
+  Buongallino Alessandro
+
+  Input di due interi che danno come output la loro somma
+
+  Input: 2 numeri interi
+  Elaborazione: Somma
+  Output: 2 Numeri Interi
+*/
+
+#include <stdio.h> //Include codici come printf e scanf
+
+int main() {
+
+  //dichiaramo le variabili di input
+  int n1, n2;
+  //e output
+  int ris;
+
+  //Input: 2 numeri interi
+  printf("Scrivi il primo numero\n");
+  scanf("%d", &n1);
+  printf("Scrivi il secondo numero\n");
+  scanf("%d", &n2);
+
+  //Output: 2 Numeri Interi
+  ris=n1+n2;
+  printf("Il risultato e' %d", ris);
+}
+
